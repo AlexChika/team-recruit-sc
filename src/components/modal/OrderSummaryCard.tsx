5ead7799971b0b9439b7c3f204bdf85f5bb22833
Alex Chika <mcluckey1@gmail.com>
2023-05-26 14:44:17 +0100

    added ordersummary ui complete

--- src/components/modal/OrderSummaryCard.tsx
+++ src/components/modal/OrderSummaryCard.tsx
@@ -1,14 +1,50 @@
 import styled from "styled-components";
 import CreditCard from "./CreditCard";
+import { SiApple } from "react-icons/si";
+import { MdOutlineReceiptLong } from "react-icons/md";
 
 const OrderSummaryCard = () => {
   return (
     <Wrapper>
       <CreditCard />
 
-      {/* summarry detaills */}
+      {/* order summarry  */}
       <div className="order__summary">
-        <h1>see ssee see see see see see see see</h1>
+        <div className="detail">
+          <h3>Company</h3>
+          <p>
+            <SiApple /> &nbsp; Apple
+          </p>
+        </div>
+
+        <div className="detail">
+          <h3>Order Number</h3>
+          <p>123456</p>
+        </div>
+
+        <div className="detail">
+          <h3>Product</h3>
+          <p>MacBook Air</p>
+        </div>
+
+        <div className="detail">
+          <h3>VAT (20%)</h3>
+          <p>$100</p>
+        </div>
+      </div>
+
+      {/* Order total */}
+      <div className="order__total">
+        <div>
+          <h3>You have to pay</h3>
+          <h2>
+            549 <span>USD</span>
+          </h2>
+        </div>
+
+        <span className="receipt__icon fcenter">
+          <MdOutlineReceiptLong />
+        </span>
       </div>
     </Wrapper>
   );
@@ -23,9 +59,78 @@ const Wrapper = styled.div`
   background-color: #cce6e623;
   border-radius: inherit;
 
+  /* General styled */
+  h3 {
+    color: var(--gray-color);
+    font-weight: 500;
+    font-size: 13px;
+  }
+
+  p {
+    display: flex;
+    align-items: center;
+    color: var(--black-color);
+    font-size: clamp(1.3rem, 3.5vw, 1.5rem);
+    font-weight: 700;
+  }
+
+  /* General styled */
+
   .order__summary {
     padding-top: calc(250px - 60px);
     width: 100%;
+    padding-bottom: 15px;
+    border-bottom: 2px dotted var(--gray-color);
+    position: relative;
+
+    .detail {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-top: 10px;
+    }
+  }
+
+  .order__summary:before,
+  .order__summary:after {
+    content: "";
+    position: absolute;
+    width: 50px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+  }
+
+  .order__summary:before {
+    right: -50px;
+    bottom: -20px;
+  }
+
+  .order__summary:after {
+    left: -50px;
+    bottom: -20px;
+  }
+
+  .order__total {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 20px;
+
+    h2 {
+      color: var(--black-color);
+      font-size: clamp(1.7rem, 3.5vw, 2rem);
+      font-weight: 900;
+      padding-top: 5px;
+
+      span {
+        color: var(--gray-color);
+      }
+    }
+  }
+
+  .receipt__icon {
+    font-size: 2.5rem;
   }
 
   @media screen and (min-width: 768px) {
